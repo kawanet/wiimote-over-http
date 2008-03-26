bb60bd5c7ee7753ebd27bcfd6bfadd146501552a
nemoto.hrs <nemoto.hrs@a0251768-1b47-0410-b396-db598902b960>
2008-03-26 13:46:51 +0000

    ソースディレクトリ変更

--- WiimoteOverHTTP/src/org/mtl/wiimote/exception/IlligalParameterException.java
+++
@@ -1,6 +0,0 @@
-package org.mtl.wiimote.exception;
-
-public class IlligalParameterException extends Exception {
-	/** Default Serial Version */
-	private static final long serialVersionUID = 1L;
-}
